e2f15a19d94c5818c4eb76d873e730db84ee8aa6
Yagmur Yilmaz <yagmurylmazx@gmail.com>
2026-03-01 02:30:43 -0800

    worker1 and worker2 logic

--- interprocess/worker_s2.c
+++ interprocess/worker_s2.c
@@ -45,7 +45,66 @@ int main (int argc, char * argv[])
     //      - write the results to the Rsp message queue
     //    until there are no more tasks to do
     //  * close the message queues
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s <Rsp_queue_name> <S2_queue_name>\n", argv[0]);
+        return 1;
+    }
 
+    const char *rsp_name = argv[1];
+    const char *s2_name = argv[2];
+
+    //open S1 queue for reading
+    mqd_t s2_mq = mq_open(s2_name, O_RDONLY);
+    if (s2_mq == (mqd_t)-1) {
+        perror("worker_s2: mq_open S2");
+        return 1;
+    }
+    
+    //open Rsp queue for writing
+    mqd_t rsp_mq = mq_open(rsp_name, O_WRONLY);
+    if (rsp_mq == (mqd_t)-1) {
+        perror("worker_s2: mq_open Rsp");
+        mq_close(s2_mq);
+        return 1;
+    }
+
+    //repeatedly: receive job -> do service1 -> rsleep -> send result
+    while(1) {
+        MQ_WORKER_MESSAGE job;
+        ssize_t n = mq_receive(s2_mq, (char *)&job, sizeof(job), NULL);
+        if (n < 0) {
+            if (errno == EINTR) continue; //retry if interrupted
+            perror("worker_s2: mq_receive");
+            break;
+        }
+
+        if((size_t)n != sizeof(MQ_WORKER_MESSAGE)) {
+            fprintf(stderr, "worker_s2: wrong message size %zd (expected %zu)\n", n, sizeof(MQ_WORKER_MESSAGE));
+            continue;
+        }
+
+        if (job.jobID < 0) {
+            break;
+        }
+
+        int res = service2(job.data);
+
+        //required random sleep between receiving and sending
+        rsleep(10000);
+
+        MQ_RESPONSE_MESSAGE resp;
+        resp.jobID = job.jobID;
+        resp.result = res;
+
+        if(mq_send(rsp_mq, (const char *)&resp, sizeof(resp), 0) == -1) {
+            if (errno == EINTR) continue; 
+            perror("worker_s2: mq_send");
+            break;   
+        }
+    }
+
+    mq_close(s2_mq);
+    mq_close(rsp_mq);
     return(0);
 }
 
