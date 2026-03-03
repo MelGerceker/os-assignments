254ae3458193acac5e9d153671751ee18d8e1b0a
Melisa <melgerceker@gmail.com>
2026-03-03 04:14:28 -0800

    added student numbers

--- interprocess/worker_s2.c
+++ interprocess/worker_s2.c
@@ -2,8 +2,9 @@
  * Operating Systems  (2INCO)  Practical Assignment
  * Interprocess Communication
  *
- * STUDENT_NAME_1 (STUDENT_NR_1)
- * STUDENT_NAME_2 (STUDENT_NR_2)
+ * STUDENT_NAME_1 (2134160)
+ * STUDENT_NAME_2 (2107724)
+ * STUDENT_NAME_3 (2157470)
  *
  * Grading:
  * Your work will be evaluated based on the following criteria:
